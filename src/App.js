ef8e979df521e1835f88dd67df532f4722da71b6
rdiazca <rdiazca96@gmail.com>
2022-04-01 21:24:24 -0400

    Creando primer componente utilizando useState

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  //useState retorna 2 funciones
+  //1-El state actual = this.state
+  //2-Función que actualiza el state this.setState()
+  const [citas, guardarCita] = useState([]);
+
+  return(
+    <h1>Hola</h1>
+  )
 }
 
 export default App;
